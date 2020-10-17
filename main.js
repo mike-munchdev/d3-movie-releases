233d68ef4cac397095cbec34e9371b565ec515cf
Michael Griffin <mike@munchdevelopment.com>
2020-10-17 17:20:14 -0500

    genres & to top

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const API_KEY = '';
+const API_KEY = '17fcd7577118002fbd3187179fc8ad61';
 
-(async ($d3, $moment, $document) => {
+(async ($d3, $genres, $months, $headers) => {
   const rangeOfYears = (start, end) =>
     Array(end - start + 1)
       .fill(start)
@@ -24,19 +24,22 @@ const API_KEY = '';
 
   const minYear = 1950;
   const maxYear = new Date().getFullYear();
+
+  // create year select list
   $d3
     .select('#year')
     .append('span')
-    .attr('class', 'year-label')
+    .attr('class', 'select-label')
     .text('Select Year:');
+
   const yearSelect = $d3
     .select('#year')
     .append('select')
     .attr('id', 'yearSelect')
     .on('change', function (event) {
+      const currentGenre = document.getElementById('genreSelect').value;
       const currentYear = document.getElementById('yearSelect').value;
-
-      getMovieDataForYear(currentYear);
+      getMovieDataForYear(currentYear, currentGenre);
     });
 
   yearSelect
@@ -51,30 +54,45 @@ const API_KEY = '';
       return d;
     });
 
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
+  // create genre select list
+  $d3
+    .select('#genre')
+    .append('span')
+    .attr('class', 'select-label')
+    .text('Select Genre:');
 
-  const getMovieDataForYear = async (year) => {
+  const genreSelect = $d3
+    .select('#genre')
+    .append('select')
+    .attr('id', 'genreSelect')
+    .on('change', function (event) {
+      const currentGenre = document.getElementById('genreSelect').value;
+      const currentYear = document.getElementById('yearSelect').value;
+      getMovieDataForYear(currentYear, currentGenre);
+    });
+
+  genreSelect
+    .selectAll('option')
+    .data($genres)
+    .enter()
+    .append('option')
+    .text(function (d) {
+      return d.name;
+    })
+    .attr('value', function (d) {
+      return d.id;
+    });
+
+  const getMovieDataForYear = async (year, genre) => {
     // clear
     $d3.select('#months').html('');
     $d3.select('#movieData').html('');
 
     const monthsElement = $d3.select('#months');
+
     monthsElement
       .selectAll('div')
-      .data(months)
+      .data($months)
       .enter()
       .append('a')
       .attr('class', 'month-name')
@@ -85,12 +103,14 @@ const API_KEY = '';
         return d;
       });
 
-    await asyncForEach(months, async (month, index) => {
+    await asyncForEach($months, async (month, index) => {
       const movieDataElement = $d3.select('#movieData').append('div');
       try {
         movieDataElement.append(() => createLoadingAnimation().node());
 
-        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&region=US&with_release_type=3|2&primary_release_date.gte=${year}-${(
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}${
+          genre ? `&with_genres=${genre}` : ''
+        }&region=US&with_release_type=3|2&primary_release_date.gte=${year}-${(
           '0' +
           (index + 1)
         ).slice(-2)}-01&primary_release_date.lte=${year}-${(
@@ -107,35 +127,40 @@ const API_KEY = '';
           .attr('id', `${month}-${year}`)
           .attr('class', 'month-container');
 
-        monthDiv.append('h2').text(`${month} ${year}`);
+        const monthDivHeader = monthDiv
+          .append('div')
+          .attr('class', 'month-header');
+        monthDivHeader
+          .append('div')
+          .attr('class', 'header-container')
+          .append('h2')
+          .text(`${month} ${year}`);
+
+        monthDivHeader.append('a').attr('href', '#top').text('top');
+
         const table = monthDiv
           .append('table')
           .style('border-collapse', 'collapse')
           .style('border', '2px black solid')
           .attr('class', 'movie-table');
-        const desiredHeaders = [
-          'id',
-          'title',
-          'release_date',
-          'vote_count',
-          'vote_average',
-        ];
+
         const filteredMovieData = movieData.results.map((d) => {
-          return desiredHeaders.reduce((acc, curr) => {
+          return $headers.reduce((acc, curr) => {
             acc[curr] = d[curr];
             return acc;
           }, {});
         });
 
+        // add table headers
         table
           .append('thead')
           .append('tr')
           .selectAll('th')
-          .data(desiredHeaders)
+          .data($headers)
           .enter()
           .append('th')
           .text(function (d) {
-            return d;
+            return d === 'genre_ids' ? 'genres' : d.replace('_', ' ');
           })
           .style('border', '1px black solid')
           .style('padding', '5px')
@@ -143,7 +168,7 @@ const API_KEY = '';
           .style('font-weight', 'bold')
           .style('text-transform', 'uppercase');
 
-        // data
+        // add table data
         table
           .append('tbody')
           .selectAll('tr')
@@ -164,11 +189,14 @@ const API_KEY = '';
           .on('mouseout', function () {
             $d3.select(this).style('background-color', 'white');
           })
-          .text(function (d) {
-            return d;
+          .text(function (d, index) {
+            return index === $headers.length - 1
+              ? d.map((i) => $genres.find((g) => g.id === i).name).join(', ')
+              : d;
           })
           .style('font-size', '12px');
       } catch (error) {
+        // display error
         movieDataElement.selectAll('.lds-facebook').remove();
         $d3
           .select('#movieData')
@@ -177,5 +205,7 @@ const API_KEY = '';
       }
     });
   };
-  await getMovieDataForYear(2020);
-})(d3, moment, document);
+
+  // get initial data
+  await getMovieDataForYear(2020, $genres[0].id);
+})(d3, genres, months, desiredHeaders);
